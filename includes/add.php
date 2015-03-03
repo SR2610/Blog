eb3825e9198ba106c63d74bac1c0c92b06967621
Stephen Ray <SR2610@users.noreply.github.com>
2015-03-03 22:05:32 +0000

    Fixes broken HTML tags in posts
    Prevents broken HTML tags from seeping into the rest of the page, the Tidy function tidies it up.It also prevents XSS as a side effect.

--- includes/add.php
+++ includes/add.php
@@ -3,14 +3,22 @@
 	include("dbInfo.php");
 	$conn = mysqli_connect("localhost",DBUSERNAME,DBPASSWORD,DB);
 	
+
+$html=$_POST["content"];
+$tidy = new Tidy();
+$clean = $tidy->repairString($html, array(
+    "output-xml" => true,
+    "input-xml" => true));
+
+
 	$title=$_POST["title"];
-	$content=$_POST["content"];
+	$content=$clean;
 	$username=$_SESSION["username"];
 	$date = getdate();
 	$datef = $date['year']."-".$date['mon']."-".$date['mday'];
 	
 	$command="INSERT INTO `".DB."`.`Posts` (`ID`, `Title`, `Content`, `Author`, `Date`) VALUES (NULL, '$title', '$content', '$username', '$datef')";
-	
+		
 	if(mysqli_query($conn,$command)){
 	header("location:../index.php");
 	}
